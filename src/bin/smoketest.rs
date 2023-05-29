5a7c48608a9425de9e5227e4e192d7b348a6fd42
Max Marcon <maxmarcon@gmx.net>
2023-05-29 10:11:07 +0200

    pass connection handler to Server as closure

--- src/bin/smoketest.rs
+++ src/bin/smoketest.rs
@@ -1,18 +1,26 @@
 use protohackers::{CliArgs, Parser, Server};
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::sync::{Arc, RwLock};
 
 fn main() {
     let args = CliArgs::parse();
 
-    Server::new(handle_stream, 5).start(args.port);
+    let distr = Arc::new(RwLock::new(HashMap::new()));
+    let handler: Arc<dyn Fn(TcpStream) + Send + Sync + 'static> = Arc::new(move |tcpstream| {
+        handle_stream(tcpstream, &distr);
+    });
+
+    Server::new(args.port, args.max_connections).serve(handler);
 }
 
-fn handle_stream(mut stream: TcpStream) {
+fn handle_stream(mut stream: TcpStream, distr: &Arc<RwLock<HashMap<usize, u32>>>) {
     let mut buf = [0; 1024];
-
     loop {
         let size = stream.read(&mut buf).unwrap();
+        let mut distr = distr.write().unwrap();
+        *distr.entry(size).or_default() += 1;
         if size == 0 {
             println!("connection closed");
             return;
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,57 @@
 pub use clap::Parser;
+use std::collections::VecDeque;
 use std::net::{TcpListener, TcpStream};
+use std::sync::Arc;
 use std::thread;
+use std::thread::JoinHandle;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct CliArgs {
     /// Port to listen to
-    #[arg(short, long, help = "Port to listen to", default_value_t = 3333)]
+    #[arg(short, long, help = "Port to listen to", default_value_t = 33333)]
     pub port: u16,
+
+    #[arg(
+        short = 'c',
+        long,
+        help = "Max concurrent connections",
+        default_value_t = 5
+    )]
+    pub max_connections: u16,
 }
 
 pub struct Server {
+    port: u16,
     max_connections: u16,
-    handler: fn(TcpStream),
 }
 
 impl Server {
-    pub fn new(handler: fn(TcpStream), max_connections: u16) -> Self {
+    pub fn new(port: u16, max_connections: u16) -> Self {
         Self {
-            handler,
+            port,
             max_connections,
         }
     }
 
-    pub fn start(&mut self, port: u16) {
-        let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).unwrap();
+    pub fn serve(&self, handler: Arc<dyn Fn(TcpStream) + Send + Sync>) {
+        let listener = TcpListener::bind(format!("0.0.0.0:{}", self.port)).unwrap();
         println!("listening on: {:?}", listener.local_addr().unwrap());
 
+        let mut join_handles = VecDeque::new();
+
         for stream in listener.incoming() {
+            while join_handles.len() >= self.max_connections as usize {
+                let top_thread: JoinHandle<()> = join_handles.pop_front().unwrap();
+                top_thread.join().unwrap();
+            }
+
             match stream {
                 Ok(stream) => {
+                    let handler = Arc::clone(&handler);
                     println!("handling connection from: {}", stream.peer_addr().unwrap());
-                    let handler = self.handler.clone();
-                    thread::spawn(move || (handler)(stream));
+                    join_handles.push_back(thread::spawn(move || (handler)(stream)));
                 }
                 Err(err) => {
                     println!("Connection failed: {:?}", err);
