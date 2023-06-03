6cda54d0c6e61c5a1ce0c61cc9be6826c4bdbab2
Max Marcon <maxmarcon@gmx.net>
2023-06-03 22:37:49 +0200

    refactor

--- src/bin/primetime.rs
+++ src/bin/primetime.rs
@@ -1,7 +1,7 @@
 use protohackers::{CliArgs, Parser, Server};
 use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::sync::{Arc};
+use std::sync::Arc;
 
 fn main() {
     let args = CliArgs::parse();
@@ -36,7 +36,7 @@ fn handle_stream(mut tcpstream: TcpStream) {
                 last = pos;
                 buffer.append(&mut vec![0_u8; READ_SIZE]);
             }
-            Err(()) => {
+            Err(_) => {
                 tcpstream.write_all(b"whats up?\n").unwrap();
                 break;
             }
@@ -46,11 +46,22 @@ fn handle_stream(mut tcpstream: TcpStream) {
 
 type JsonMap = serde_json::Map<String, serde_json::Value>;
 
-fn read_json_object(bytes: &[u8]) -> Result<(Option<JsonMap>, usize), ()> {
+enum DecodeError {
+    Serde(serde_json::Error),
+    UnbalancedBrackets,
+}
+
+impl From<serde_json::Error> for DecodeError {
+    fn from(error: serde_json::Error) -> Self {
+        Self::Serde(error)
+    }
+}
+
+fn read_json_object(bytes: &[u8]) -> Result<(Option<JsonMap>, usize), DecodeError> {
     let mut open_brackets = 0;
     for (pos, byte) in bytes.iter().enumerate() {
         if pos == 0 && *byte != b'{' {
-            return Err(());
+            return Err(DecodeError::UnbalancedBrackets);
         }
         if *byte == b'{' {
             open_brackets += 1;
@@ -58,16 +69,13 @@ fn read_json_object(bytes: &[u8]) -> Result<(Option<JsonMap>, usize), ()> {
         if *byte == b'}' {
             open_brackets -= 1;
         }
-
         if open_brackets < 0 {
-            return Err(());
+            return Err(DecodeError::UnbalancedBrackets);
         }
 
-        if open_brackets == 0 {
-            match serde_json::from_slice(&bytes[..=pos]) {
-                Err(_) => return Err(()),
-                Ok(json) => return Ok((Some(json), pos + 1)),
-            }
+        if open_brackets <= 0 {
+            let json = serde_json::from_slice(&bytes[..=pos])?;
+            return Ok((Some(json), pos + 1));
         }
     }
 
