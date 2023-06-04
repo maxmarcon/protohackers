1129cee15b8e85cb5f3c0f3aa3ff43e6cae1ef6c
Max Marcon <maxmarcon@gmx.net>
2023-06-04 21:57:49 +0200

    implemented primetime

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Protohackers Challenge
+
+Solutions to the [protohackers](https://protohackers.com) challenge.
+
+Each problem's solution is implemented as a separate binary crate named after the problem:
+
+```bash
+cargo run --bin smoketest
+cargo run --bin primetime
+```
+
+Run ```cargo run --bin <crate> -- -h``` to see a list of options.
--- src/bin/primetime.rs
+++ src/bin/primetime.rs
@@ -1,7 +1,22 @@
 use protohackers::{CliArgs, Parser, Server};
+use serde_json::{json, value::Value, Map};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::sync::Arc;
+
+#[derive(Debug)]
+enum MessageError {
+    Serde(serde_json::Error),
+    UnbalancedBrackets,
+    NotAMap,
+    Invalid,
+}
+
+impl From<serde_json::Error> for MessageError {
+    fn from(error: serde_json::Error) -> Self {
+        Self::Serde(error)
+    }
+}
 
 fn main() {
     let args = CliArgs::parse();
@@ -26,48 +41,45 @@ fn handle_stream(mut tcpstream: TcpStream) {
         if read == 0 {
             break;
         }
-        
+
         write_from += read;
-        match read_json_object(&buffer[0..write_from]) {
-            Ok((Some(json), pos)) => {
-                write_from = 0;
-                buffer.drain(0..pos);
-                if buffer.is_empty() {
+
+        let response = parse_message(&buffer[0..write_from])
+            .map(|(json, pos)| {
+                if json.is_some() {
+                    write_from = 0;
+                    buffer.drain(0..pos);
+                    if buffer.is_empty() {
+                        buffer.append(&mut vec![0_u8; READ_CHUNK]);
+                    }
+                } else if write_from == buffer.len() {
                     buffer.append(&mut vec![0_u8; READ_CHUNK]);
                 }
-            }
-            Ok((None, _pos)) => {
-                if write_from == buffer.len() {
-                    buffer.append(&mut vec![0_u8; READ_CHUNK]);
-                }
-            }
+                json
+            })
+            .and_then(|json| json.map_or(Ok(None), |json| process_message(json).map(Some)));
+
+        println!("response = {:?}", response);
+        match response {
+            Ok(Some(response)) => tcpstream
+                .write_all(serde_json::to_string(&response).unwrap().as_bytes())
+                .unwrap(),
+            Ok(None) => (),
             Err(e) => {
-                tcpstream.write_all(format!("{e:?}").as_bytes()).unwrap();
+                tcpstream
+                    .write_all(json!({ "error": format!("{e:?}") }).to_string().as_bytes())
+                    .unwrap();
                 break;
             }
         }
     }
 }
 
-type JsonMap = serde_json::Map<String, serde_json::Value>;
-
-#[derive(Debug)]
-enum DecodeError {
-    Serde(serde_json::Error),
-    UnbalancedBrackets,
-}
-
-impl From<serde_json::Error> for DecodeError {
-    fn from(error: serde_json::Error) -> Self {
-        Self::Serde(error)
-    }
-}
-
-fn read_json_object(bytes: &[u8]) -> Result<(Option<JsonMap>, usize), DecodeError> {
+fn parse_message(bytes: &[u8]) -> Result<(Option<Map<String, Value>>, usize), MessageError> {
     let mut open_brackets = 0;
     for (pos, byte) in bytes.iter().enumerate() {
         if pos == 0 && *byte != b'{' {
-            return Err(DecodeError::UnbalancedBrackets);
+            return Err(MessageError::UnbalancedBrackets);
         }
         if *byte == b'{' {
             open_brackets += 1;
@@ -76,14 +88,69 @@ fn read_json_object(bytes: &[u8]) -> Result<(Option<JsonMap>, usize), DecodeErro
             open_brackets -= 1;
         }
         if open_brackets < 0 {
-            return Err(DecodeError::UnbalancedBrackets);
+            return Err(MessageError::UnbalancedBrackets);
         }
 
         if open_brackets <= 0 {
             let json = serde_json::from_slice(&bytes[..=pos])?;
-            return Ok((Some(json), pos + 1));
+            return match json {
+                Value::Object(map) => Ok((Some(map), pos + 1)),
+                _ => Err(MessageError::NotAMap),
+            };
         }
     }
 
     Ok((None, bytes.len()))
+}
+
+fn process_message(message: Map<String, Value>) -> Result<Value, MessageError> {
+    println!("processing: {:?}", message);
+    match message.get("method") {
+        None => return Err(MessageError::Invalid),
+        Some(value) if value != "isPrime" => return Err(MessageError::Invalid),
+        _ => (),
+    }
+
+    let is_prime = match message.get("number") {
+        None => return Err(MessageError::Invalid),
+        Some(value) if !value.is_number() => return Err(MessageError::Invalid),
+        Some(number) if number.is_f64() => None,
+        Some(number) if number.is_u64() => number.as_u64(),
+        _ => None,
+    }
+    .map_or(false, is_prime);
+
+    Ok(json!({
+        "method": "isPrime",
+        "prime": is_prime
+    }))
+}
+
+fn is_prime(n: u64) -> bool {
+    let sq = (n as f64).sqrt() as u64;
+    for p in 2..=sq {
+        if n % p == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::is_prime;
+
+    #[test]
+    fn test_is_prime() {
+        assert!(is_prime(2));
+        assert!(is_prime(3));
+        assert!(is_prime(17));
+        assert!(is_prime(103));
+        assert!(is_prime(8297));
+
+        assert!(!is_prime(16));
+        assert!(!is_prime(22));
+        assert!(!is_prime(44));
+        assert!(!is_prime(100));
+    }
 }
