b4e0091310b459716eb1a92414466a458f32f3dd
Max Marcon <maxmarcon@gmx.net>
2023-07-17 07:44:54 +0200

    wip

--- src/lrcp.rs
+++ src/lrcp.rs
@@ -10,8 +10,9 @@ use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio::task::JoinHandle;
 
 #[derive(Debug)]
-enum Error {
+pub enum Error {
     EOF,
+    Disconnected,
     IO(io::Error),
 }
 
@@ -42,7 +43,7 @@ impl Socket {
         if let Some(stream) = self.stream_receiver.recv().await {
             Ok(stream)
         } else {
-            Err(Error::EOF)
+            Err(Error::Disconnected)
         }
     }
 }
@@ -55,27 +56,62 @@ impl Drop for Socket {
 
 #[derive(Debug)]
 pub struct Stream {
-    app_reader: Receiver<String>,
-    app_writer: Sender<String>,
+    session_id: i32,
+    app_reader: Receiver<Datagram>,
+    app_writer: Sender<Datagram>,
 }
 
 impl Stream {
     pub async fn send(&mut self, data: &str) -> Result<(), Error> {
-        if self.app_writer.send(data.to_owned()).await.is_ok() {
+        if self
+            .app_writer
+            .send(Datagram {
+                session_id: self.session_id,
+                data: data.to_owned(),
+            })
+            .await
+            .is_ok()
+        {
             Ok(())
         } else {
-            Err(Error::EOF)
+            Err(Error::Disconnected)
         }
     }
+
+    pub async fn read(&mut self) -> Result<String, Error> {
+        let mut all_data;
+        if let Some(datagram) = self.app_reader.recv().await {
+            all_data = datagram.data;
+        } else {
+            return Err(Error::EOF);
+        }
+        loop {
+            match self.app_reader.try_recv() {
+                Ok(Datagram { data, session_id }) => {
+                    all_data += &data;
+                }
+                Err(TryRecvError::Empty) => break,
+                Err(_) => return Err(Error::EOF),
+            }
+        }
+        Ok(all_data)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Datagram {
+    pub data: String,
+    pub session_id: i32,
 }
 
 struct Session {
     id: i32,
-    acked: i32,
+    // last ack sent to peer
+    last_ack_sent: i32,
     peer: SocketAddr,
-    pos: i32,
-    to_app: Sender<String>,
-    from_app: Receiver<String>,
+    // starting position of next data to send to peer
+    next_pos: i32,
+    to_stream: Sender<Datagram>,
 }
 
 struct SocketState {
@@ -86,64 +122,67 @@ struct SocketState {
 
 impl SocketState {
     async fn protocol_loop(&mut self) -> io::Result<()> {
+        let (app_writer, mut from_stream) = channel(256);
         loop {
-            // is there data from the application layer?
-            for (session_id, session) in self.session_store.iter_mut() {
-                match session.from_app.try_recv() {
-                    Ok(data) => {
-                        self.udpsocket
-                            .send_to(
-                                Data::new(*session_id, session.pos, &data)
-                                    .encode()
-                                    .as_bytes(),
-                                session.peer,
-                            )
-                            .await?;
-                        session.pos += data.len() as i32;
-                    }
-                    Err(TryRecvError::Empty) => (),
-                    Err(_) => {
-                        // close session
+            let mut buf = Vec::new();
+            tokio::select! {
+                result = self.udpsocket.recv_buf_from(&mut buf) => {
+                    let (_size, addr) = result?;
+                    self.process_udp(&buf, addr, &app_writer).await?;
+                }
+                Some(datagram) = from_stream.recv() => {
+                     if let Some(session) = self.session_store.get_mut(&datagram.session_id) {
+                         self.udpsocket
+                                .send_to(
+                                    Data::new(session.id, session.next_pos, &datagram.data)
+                                        .encode()
+                                        .as_bytes(),
+                                    session.peer,
+                                )
+                                .await?;
+                        session.next_pos += datagram.data.len() as i32;
                     }
                 }
             }
-
-            let mut buf = Vec::new();
-            let (_size, addr) = self.udpsocket.recv_buf_from(&mut buf).await?;
-            self.handle_udpdata(&buf, addr).await?;
         }
     }
 
     async fn ack_session(&self, session: &Session) -> io::Result<()> {
         self.udpsocket
             .send_to(
-                Ack::new(session.id, session.acked).encode().as_bytes(),
+                Ack::new(session.id, session.last_ack_sent)
+                    .encode()
+                    .as_bytes(),
                 session.peer,
             )
             .await?;
         Ok(())
     }
 
-    async fn handle_udpdata(&mut self, buf: &[u8], addr: SocketAddr) -> io::Result<()> {
+    async fn process_udp(
+        &mut self,
+        buf: &[u8],
+        addr: SocketAddr,
+        app_writer: &Sender<Datagram>,
+    ) -> io::Result<()> {
         let decoded = decode(&buf);
         match decoded {
             Ok(msg) => match msg {
                 Decoded::Connect(connect) => {
                     if self.session_store.get(&connect.session).is_none() {
                         let (to_app, app_reader) = channel(256);
-                        let (app_writer, from_app) = channel(256);
 
                         let session = Session {
                             id: connect.session,
-                            acked: 0,
-                            pos: 0,
+                            last_ack_sent: 0,
+                            next_pos: 0,
                             peer: addr,
-                            to_app,
-                            from_app,
+                            to_stream: to_app,
                         };
 
                         let stream = Stream {
-                            app_writer,
+                            session_id: session.id,
+                            app_writer: app_writer.clone(),
                             app_reader,
                         };
                         self.session_store.insert(connect.session, session);
@@ -160,11 +199,23 @@ impl SocketState {
                     }
                 }
                 Decoded::Data(data) => {
-                    self.session_store.entry(data.session).and_modify(|s| {
-                        if s.acked == data.pos {
-                            s.acked += data.data.len() as i32
+                    let mut stream_gone = false;
+                    if let Some(session) = self.session_store.get_mut(&data.session) {
+                        if session.last_ack_sent == data.pos {
+                            session.last_ack_sent += data.data.len() as i32;
+                            stream_gone = session
+                                .to_stream
+                                .send(Datagram {
+                                    session_id: session.id,
+                                    data: data.data,
+                                })
+                                .await
+                                .is_err();
                         }
-                    });
+                    };
+                    if stream_gone {
+                        self.session_store.remove(&data.session);
+                    }
                     if let Some(session) = self.session_store.get(&data.session) {
                         self.ack_session(&self.session_store[&data.session]).await?;
                     } else {
@@ -221,7 +272,7 @@ mod tests {
 
     #[tokio::test]
     async fn peer_sends_data_for_open_session() {
-        let (_socket, peer, _stream) = open_session().await;
+        let (_socket, peer, mut stream) = open_session().await;
         peer.send(format!("/data/{SESSION}/0/hello/").as_bytes())
             .await
             .unwrap();
@@ -229,11 +280,15 @@ mod tests {
         let mut buf: Vec<u8> = Vec::new();
         peer.recv_buf(&mut buf).await.unwrap();
         assert_eq!(buf, format!("/ack/{SESSION}/5/").as_bytes());
+
+        let read_result = stream.read().await;
+        assert!(read_result.is_ok());
+        assert_eq!(read_result.unwrap(), "hello");
     }
 
     #[tokio::test]
     async fn peer_sends_data_beyond_what_already_acked() {
-        let (_socket, peer, _stream) = open_session().await;
+        let (_socket, peer, mut stream) = open_session().await;
         peer.send(format!("/data/{SESSION}/0/hello/").as_bytes())
             .await
             .unwrap();
@@ -246,17 +301,39 @@ mod tests {
         buf.clear();
         peer.recv_buf(&mut buf).await.unwrap();
         assert_eq!(buf, format!("/ack/{SESSION}/5/").as_bytes());
+
+        let read_result = stream.read().await;
+        assert!(read_result.is_ok());
+        assert_eq!(read_result.unwrap(), "hello");
     }
 
     #[tokio::test]
-    async fn sending_data_to_peer() {
+    async fn multiple_data_packets_from_peer_are_combined_in_stream() {
+        let (_socket, peer, mut stream) = open_session().await;
+        peer.send(format!("/data/{SESSION}/0/hello/").as_bytes())
+            .await
+            .unwrap();
+        peer.send(format!("/data/{SESSION}/5/ world/").as_bytes())
+            .await
+            .unwrap();
+
+        let read_result = stream.read().await;
+        assert!(read_result.is_ok());
+        assert_eq!(read_result.unwrap(), "hello world");
+    }
+
+    #[tokio::test]
+    async fn local_end_sends_data_to_peer() {
         let (_socket, peer, mut stream) = open_session().await;
 
         assert!(stream.send("hello world").await.is_ok());
 
         let mut buf: Vec<u8> = Vec::new();
         peer.recv_buf(&mut buf).await.unwrap();
-        assert_eq!(buf, format!("/data/hello world/11/").as_bytes());
+        assert_eq!(
+            String::from_utf8(buf).unwrap(),
+            format!("/data/{SESSION}/0/hello world/")
+        );
     }
 
     async fn open_session() -> (Socket, UdpSocket, Stream) {
